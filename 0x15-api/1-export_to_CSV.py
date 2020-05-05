a0e8b90437206b62329bc98a4385d8c4fc955ef4
Feli <srgatoiscool@gmail.com>
2020-05-05 02:37:16 +0000

    add

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,11 +3,11 @@
 for a given employee ID,
 returns information about his/her TODO list progress."""
 
-import csv
-import requests
-import sys
-
 if __name__ == "__main__":
+
+    import csv
+    import requests
+    import sys
 
     userId = sys.argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     name = user.json().get('username')
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-    filename = userId + '.csv'
-    with open(filename, mode='w') as f:
+    filenam = userId + '.csv'
+    with open(filenam, mode='w') as f:
         writer = csv.writer(f, delimiter=',', quotechar='"',
                             quoting=csv.QUOTE_ALL, lineterminator='\n')
         for task in todos.json():
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-"""Write a Python script that, using this REST API
-for a given employee ID,
-returns information about his/her TODO list progress."""
-
-import json
-import requests
-import sys
+"""Exports data in the JSON format"""
 
 if __name__ == "__main__":
 
+    import json
+    import requests
+    import sys
+
     userId = sys.argv[1]
-    api = 'https://jsonplaceholder.typicode.com'
-    user = requests.get(api+'/users/'+userId)
-
-    name = user.json().get('name')
-
-    todos = requests.get(api+'/todos').json()
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
 
     todoUser = {}
     taskList = []
 
     for task in todos:
         if task.get('userId') == int(userId):
-            __task = {"task": task.get('title'),
-                      "completed": task.get('completed'),
-                      "username": user.json().get('username')}
-            taskList.append(__task)
+            taskDic = {"task": task.get('title'),
+                       "completed": task.get('completed'),
+                       "username": user.json().get('username')}
+            taskList.append(taskDic)
     todoUser[userId] = taskList
 
     filename = userId + '.json'
