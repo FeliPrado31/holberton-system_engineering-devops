47ce49061efd912eca0f3aaa3f051e31c7a9dce6
Feli <srgatoiscool@gmail.com>
2020-05-08 03:48:29 +0000

    add

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,34 +4,22 @@ Write a recursive function that queries the Reddit API and returns a list
 containing the titles of all hot articles for a given subreddit. If no results
 are found for the given subreddit, the function should return None.
 """
-import requests
+from requests import *
 
 
-def recurse(subreddit, data_list=[], after=None):
-    if type(subreddit) is not str:
+def recurse(subreddit, res_list=[], after=""):
+
+    url = 'https://api.reddit.com/r/{}/hot?after={}'.format(subreddit, after)
+    headers = {'user-agent': 'chrome:happy/0.1.0'}
+
+    response = get(url, headers=headers).json()
+
+    try:
+        for data in response.get('data')['children']:
+            res_list.append(data['data']['title'])
+        if response['data']['after'] is not None:
+            recurse(subreddit, res_list, response['data']['after'])
+        return res_list
+    except Exception:
         return None
-    tmp = subreddit
-    api_url = "https://api.reddit.com/r/{}/hot?after={}".format(tmp, after)
-    headers = {'user-agent': 'safari:holberton/0.1.0'}
-    res = requests.get(api_url, headers=headers)
-    if res.status_code == 200:
-        data = res.json()["data"]["children"]
-        after = res.json()["data"]["after"]
-        if after is None:
-            data_list = titles(data, len(data))
-            return data_list
-        data_list.append(recurse(subreddit, data_list, after=after))
-        data_list = titles(data, len(data))
-    else:
-        return None
-    return data_list
 
-
-def titles(data_list, length, arr=[]):
-    """
-    Gets titles of posts from the data
-    """
-    if length == 0:
-        return arr
-    arr.append(arr[length - 1]["data"]["title"])
-    return titles(arr, length - 1, arr)
